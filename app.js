39311e999db5859e7df164216700717bf2e78d30
alaanaldous <alaanaldous@gmail.com>
2020-09-14 14:43:34 +0300

    not-finish

--- app.js
+++ app.js
@@ -1,41 +1,46 @@
 'use strict';
-var hours = ['6 am','7 am', '8 am','9 am' , '10 am ' , '11 am ', '12 am' , '1 pm' , '2 pm ', '3 pm' , '4 pm' , '5 pm' , '6 pm' , '7 pm'];
-function getRandomNumber(min, max){
-  var random =math.random();
-  random = (random*(max-min+1)+min);
-  random = math.floor(random);
-  return random;
+
+var workHours =['6Am','7Am','8Am','9Am','10Am','11Am','12Pm','1Pm','2Pm','3Pm','4Pm','5Pm','6Pm','7Pm'];
+
+function getRandomNumber(min , max){
+    var random =math.random();
+    random = (random *(max-min+1))+min;
+    random =math.floor(random);
+    return random;
 }
 
-var seattle = {
-    location : 'seattle',
-    minCust: 23,
-    maxCust: 65,
-    avgSale : 6.3,
-    results: [],
-    total: 0,
-    gitCust: function(){
-        for (var i =0; i< hours.length; i++)
-        this.results[i]= getRandomNumber(this.min, this.max);
-        
-    },
+var seattle ={
+    location : 'Seattle',
+    max = 65 ,
+    min = 23, 
+    result = [],
+    total = [],
+    customers= 0,
+    avgCookies= 6.3,
 
-    render: function(){
-      var parentElement = document.getElementById('seattlelist');
-      var article = document.createElement('article');
-      //parentElement.appendChile('article');
-      var h2 = document.createElement('h2');
-      h2.textContent= this.location;
-      var ul = document.createElement('ul');
-      for (var i =0; i< hours.length; i++){
-        var li = document.createElement('li');
-        li.textContent = this.results[i];
-        ul.appendChild(li);
-    }
-
+ purchasecCookies: function (){
+     var purchasecCookiesInHour = this.customers*this.avgCookies;
+     return [purchasecCookiesInHour];
+ },
+ render : function (){
+     var container = document.getElementById('main');
+     var section = document.createElement('section');
+     container.appendChild('section');
+     var head = document.createElement('h2');
+     section.appendChild('head');
+     head.textContent= this.location;
+     var unOrderedList = document.createElement('ul');
+     section.appendChild('unOrderedList');
+     for (var i=0; i < workHours.length; i++){
+         this.purchasecCookies();
+         this.getRandomNumber();
+         this.purchasecCookies();
+         var listItem = document.createElement('li');
+         unOrderedList.appendChild('listItem');
+         listItem.textContent=this.workHours[i]+' '+this.purchasecCookies();+ ' ' + 'cookies';
+     }
+ }
 }
-}
-
-seattle.gitCust();
+seattle.purchasecCookies();
 seattle.render();
-
+console.table(seattle);

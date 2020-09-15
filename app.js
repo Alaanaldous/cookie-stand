89ed0c4c158a19131101ee29aefa2b47d56fa56a
Alaanaldous <70090920+Alaanaldous@users.noreply.github.com>
2020-09-15 18:48:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ for (var i = 0; i < store.length; i++) {
 }
 store[0].totals();
 store[0].totalFun();
-=======
+//locations
 var OpenHour = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm'];
 
 var Locations =[Seattle,Tokyo,Dubai,Paris,Lima];

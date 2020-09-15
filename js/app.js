05434dd58d2a56da99728d54882d847d63829146
alaanaldous <alaanaldous@gmail.com>
2020-09-15 16:02:08 +0300

    finished

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var OpenHour = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm'];
 
 var Locations =[Seattle,Tokyo,Dubai,Paris,Lima];
@@ -371,5 +372,3 @@ function generateRandomNumber(min, max) {
   return random;
 
 }
-
-//Alaa N Aldous
